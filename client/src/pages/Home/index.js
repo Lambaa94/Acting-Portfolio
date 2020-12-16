471ad2e34ff0f3fbaec5dd884cfa9d1fa4f05c0c
James Lamb <jameslamb13@gmail.com>
2020-12-15 23:09:05 -0500

    getting the folder structure set and up and running.

---
+++ client/src/pages/Home/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+    return (
+
+     "HEY"
+    )
+    
+}
+
+export default Home;
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const session = require("express-session");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+
+
+// -------------- End Of Imports ------------------- //
+
+// --- Middleware --- //
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+
+// --- End of Middleware --- //
+
+
+app.listen(PORT, function () {
+    console.log(`🌎 ==> API server now on port ${PORT}!`);
+  });
+  
