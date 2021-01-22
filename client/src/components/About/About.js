508b7d361d8a8e247f225948c0e196abf8233783
James Lamb <jameslamb13@gmail.com>
2021-01-22 15:49:44 -0500

    added pic to about page and lorem text.

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -1,14 +1,25 @@
 
 import React from "react";
 import { Jumbotron, Button, Container, Row, Col, Image, Carousel } from "react-bootstrap";
-
+import "./About.css"
 
 function About() {
 
     return (
         <>
-
+<br/>
 <Container>
+<Row>
+  <Col>
+  <Image src="images/42C2C5D6-0BD3-4E0E-AA5F-860CCB1C8E16.JPG" alt="Hannah Ryen" id="aboutPic"></Image>
+  </Col>
+    <Col>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, in eius dolorum quidem autem praesentium tenetur eos necessitatibus consectetur libero iste quia totam natus fuga tempora rem ratione. Autem, molestiae! Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, in eius dolorum quidem autem praesentium tenetur eos necessitatibus consectetur libero iste quia totam natus fuga tempora rem ratione. Autem, molestiae! Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, in eius dolorum quidem autem praesentium tenetur eos necessitatibus consectetur libero iste quia totam natus fuga tempora rem ratione. Autem, molestiae! Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, in eius dolorum quidem autem praesentium tenetur eos necessitatibus consectetur libero iste quia totam natus fuga tempora rem ratione. Autem, molestiae! Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, in eius dolorum quidem autem praesentium tenetur eos necessitatibus consectetur libero iste quia totam natus fuga tempora rem ratione. Autem, molestiae!
+
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, in eius dolorum quidem autem praesentium tenetur eos necessitatibus consectetur libero iste quia totam natus fuga tempora rem ratione. Autem, molestiae!</p>    
+    </Col>
+
+</Row>
 </Container>
 </>
     )
