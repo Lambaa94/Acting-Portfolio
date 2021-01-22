b47fe1c364527c4d446f53c8e07811c5432a9c24
James Lamb <jameslamb13@gmail.com>
2021-01-22 16:46:37 -0500

    got some underlines off of navbar.

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
-import { Navbar, Nav } from 'react-bootstrap';
+import { Nav } from 'react-bootstrap';
 import "./Navbar.css";
 
 function Navtop() {
     
   const location = useLocation();
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top sticky-top">
+        <Nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top sticky-top">
             <a href="/" className="navbar-brand" id="nameTitle">Hannah Ryen</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -15,24 +15,24 @@ function Navtop() {
             </button>
             <div className="collapse navbar-collapse nav justify-content-end" id="navbarNav">
                 
-                    <Link className={location.pathname==="/" ? "mr-2 navbarLink active": "mr-2 navbarLink" } to="/">Home</Link>
+                    <Link className={location.pathname==="/" ? "mr-2 nav-item active": "mr-2 nav-item" } to="/">Home</Link>
                     
-                    
+                   
                   
-                    <Link className={location.pathname==="/about" ? "mr-2 navbarLink active": "mr-2 navbarLink"} to="/about">About Me</Link>
+                    <Link className={location.pathname==="/about" ? "mr-2 active": "mr-2 nav-item"} to="/about">About Me</Link>
                     
                    
-                    <Link className={location.pathname==="/resume" ? "mr-2 navbarLink active": "mr-2 navbarLink"} to="/resume">Resume</Link>
+                    <Link className={location.pathname==="/resume" ? "mr-2 active": "mr-2 nav-item"} to="/resume">Resume</Link>
                     
                    
-                    <Link className={location.pathname==="/reel" ? "mr-2 navbarLink active": "mr-2 navbarLink"} to="/reel">Reel</Link>
+                    <Link className={location.pathname==="/reel" ? "mr-2 active": "mr-2 nav-item"} to="/reel">Reel</Link>
 
                     
-                    <Link className={location.pathname==="/photos" ? "mr-2 navbarLink active": "mr-2 navbarLink"} to="/photos">Photos</Link>
-                    <Link className={location.pathname==="/contact" ? "mr-2 navbarLink active": "mr-2 navbarLink"} to="/contact">Contact Me</Link>
+                    <Link className={location.pathname==="/photos" ? "mr-2 active": "mr-2 nav-item"} to="/photos">Photos</Link>
+                    <Link className={location.pathname==="/contact" ? "mr-2 active": "mr-2 nav-item"} to="/contact">Contact Me</Link>
             
             </div>
-        </nav>
+        </Nav>
 
     )
 }
