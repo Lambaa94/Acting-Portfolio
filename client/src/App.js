ff4490884d9ed19347440ef6c69d514f2c448fc1
James Lamb <jameslamb13@gmail.com>
2021-01-24 15:32:47 -0500

    added image to home page.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "./pages/Home/Home";
 import Navtop from "../src/components/Navbar/Navbar";
 import About from "../src/pages/About/About"
 
---
+++ client/src/pages/Home/Home.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Jumbotron, Button, Container, Row, Col, Image, Carousel } from "react-bootstrap";
+import "./Home.css";
+
+
+
+function Home() {
+
+return (
+
+<>
+<br/>
+<Container>
+<Row>
+    <Col>
+<Image src="/images/img_4368.jpeg" alt="Hannah Ryen" id="homePic"></Image>
+    
+    </Col>
+</Row>
+</Container>
+
+</>
+
+);
+ 
+}
+
+export default Home;
