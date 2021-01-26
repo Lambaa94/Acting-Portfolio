d4cb38fd6041a3ac6c45ab43e2f1bf657cae49fe
James Lamb <jameslamb13@gmail.com>
2021-01-25 22:00:57 -0500

    added images to gallery page.

--- client/src/components/Gallery/Gallery.js
+++ client/src/components/Gallery/Gallery.js
@@ -1,13 +1,64 @@
 import React from "react";
+import { Jumbotron, Button, Container, Row, Col, Image, Carousel } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./Gallery.css";
+
+
 
 function Gallery() {
 
     return (
 
         <>
+        <Container>
+            <Row>
+<Col>
 
-        <h2>Gallery Page *__*</h2>
+<Image src="/images/img_4358.jpeg" alt="Hannah Ryen" className="galleryPics"></Image>
 
+</Col>
+<Col>
+<Image src="/images/702AA743-1BF8-4834-9CB6-129DF0AB2406.JPG" alt="Hannah Ryen" className="galleryPics"></Image>
+
+</Col>
+<Col>
+<Image src="/images/img_4368.jpeg" alt="Hannah Ryen" className="galleryPics"></Image>
+
+</Col>
+            </Row>
+            <Row>
+            <Col>
+            <Image src="/images/img_4373.jpeg" alt="Hannah Ryen" className="galleryPics"></Image>
+</Col>
+<Col>
+<Image src="/images/1FB77BAF-E59E-4BDE-979A-CDCB702D30C0.JPG" alt="Hannah Ryen" className="galleryPics"></Image>
+</Col>
+<Col>
+<Image src="/images/img_4379.jpeg" alt="Hannah Ryen" className="galleryPics"></Image>
+
+</Col>
+
+            </Row>
+            <Row>
+            <Col>
+            
+</Col>
+<Col>
+<Image src="/images/A8DB8D68-A494-4001-ACE6-113FE22400D1.JPG" alt="Hannah Ryen" className="galleryPics"></Image>
+</Col>
+<Col>
+
+
+</Col>
+
+            </Row>
+        
+        
+        
+        
+       
+        
+        </Container>
 
         </>
     )
