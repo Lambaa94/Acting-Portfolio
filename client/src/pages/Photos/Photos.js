d0edb08d5d6f8227b6cf6fbd8b50ffb116065943
James Lamb <jameslamb13@gmail.com>
2021-01-27 17:57:12 -0500

    added stock photos to main photo page.

--- client/src/pages/Photos/Photos.js
+++ client/src/pages/Photos/Photos.js
@@ -18,7 +18,7 @@ function Photos() {
                         <h1>Gallery</h1>
                         <Row>
                             <Col>
-                          <Link to="/gallery"> <Image src="images/566D3108-7EF6-472E-AA0D-71E030C50339.JPG" alt="Hannah Ryen" className="photosPics"></Image></Link> 
+                          <Link to="/gallery"> <Image src="/images/pexels-vitaly-vlasov-1655817.jpg" alt="Hannah Ryen" className="photosPics"></Image></Link> 
                             </Col>
                             
                         </Row>
@@ -32,7 +32,7 @@ function Photos() {
                         <h1>Performance</h1>
                         <Row>
                             <Col>
-                           <Link to="/performance"><Image src="/images/img_4368.jpeg" alt="Hannah Ryen" className="photosPics"></Image></Link> 
+                           <Link to="/performance"><Image src="/images/pexels-cottonbro-4722583.jpg" alt="Hannah Ryen" className="photosPics"></Image></Link> 
                             </Col>
                             
                         </Row>
