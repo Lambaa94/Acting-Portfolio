4371b1824571ed84b254b3981167ce7d072d46a6
James Lamb <jameslamb13@gmail.com>
2021-01-27 16:59:03 -0500

    added pics to performance page.

--- client/src/components/Performance/Performance.js
+++ client/src/components/Performance/Performance.js
@@ -1,12 +1,39 @@
 import React from "react";
+import { Jumbotron, Button, Container, Row, Col, Image, Carousel } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./Performance.css";
 
 function Performance() {
 
-
     return(
-        <>
-<h2>Performance Page @__@</h2>
-        </>
+<>
+        <Container>
+<Row>
+    <Col>
+            <Image src="/images/img_5593.jpeg" alt="Hannah Ryen" className="performancePics"></Image>
+    </Col>
+</Row>
+
+
+<Row>
+    <Col>
+            <Image src="images/IMG_5929.JPG" alt="Hannah Ryen" className="performancePics"></Image>
+    </Col>
+
+
+</Row>
+
+<Row>
+    <Col>
+            <Image src="/images/img_5572.jpeg" alt="Hannah Ryen" className="performancePics"></Image>
+    </Col>
+
+</Row>
+        
+        
+        </Container>
+
+</>
     )
 }
 
