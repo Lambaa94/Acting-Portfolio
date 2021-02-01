4dba0b39c54de3f892f7896312c6ed119f0b41b1
James Lamb <jameslamb13@gmail.com>
2021-02-01 00:33:00 -0500

    finished with setUp1 media queries and starting setUp2.

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -11,7 +11,7 @@ function Home() {
 
         <>
             <br />
-            <Container>
+            <Container id="setUp1">
                 <Row>
                     <Col>
                     <div>
@@ -49,6 +49,48 @@ function Home() {
                     </div>
                     </Col> 
                 </Row>
+            </Container>
+            <Container id="setUp2">
+                <Row >
+                    <Col >
+                    <div >
+                        <Image src="/images/img_4368.jpeg" alt="Hannah Ryen" id="homePic" ></Image>
+                    </div>
+                    </Col>
+                    </Row>
+                    <Row>
+                    <Col>
+                    <div>
+                    <Jumbotron id="homeJumbo">
+                        <h3>Recent Roles</h3>
+                        <hr />
+                        <br />
+                        Lorem ipsum dolordeserunt illum, laudantium tempora delectus cupiditate error.
+                        <br />
+                        <br />
+                        <br />
+                        <h3>Upcoming</h3>
+                        <hr />
+                        <br />
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste invenepudiandae deserunt illum, laudantium tempora delectus cupiditate error
+                        <br />
+                        <br />
+                       
+                        <br /><Row>
+                            <Col>
+                            <Link to="/contact">
+                                <Button id="contactMe">Contact Me</Button></Link>
+
+                            </Col>
+                            <Col>
+                                <span><i class="fab fa-3x fa-instagram"></i></span>{" "}<span><i class="fab fa-3x fa-youtube m-2"></i></span>{" "}<span><i class="fab fa-3x fa-twitter-square"></i></span>
+                            </Col>
+                        </Row>
+                    </Jumbotron>
+                    </div>
+                    </Col> 
+                    </Row>
+               
             </Container>
 
         </>
