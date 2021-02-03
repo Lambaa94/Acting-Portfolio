b3bcb0ddd80ef45d9f503894b552f62c3ee96684
James Lamb <jameslamb13@gmail.com>
2021-02-02 22:40:10 -0500

    working on resume media queries.

--- client/src/pages/Resume/Resume.js
+++ client/src/pages/Resume/Resume.js
@@ -8,7 +8,7 @@ function Resume() {
 <>
 <br/>
 
-<Container>
+<Container className="setUp1">
 <Row>
 <Col>
 <Image src="/images/img_4379.jpeg" alt="Hannah Ryen" id="resumePic"></Image>
@@ -144,6 +144,144 @@ function Resume() {
 
 
 </Container>
+
+<Container className="setUp2">
+<Row>
+<Col>
+<Image src="/images/img_4379.jpeg" alt="Hannah Ryen" id="resumePic"></Image>
+
+</Col>
+<Jumbotron id="resumeJumbo">
+<Col>
+
+<span ><i className="far fa-3x fa-file-pdf"></i></span>
+
+<br/>
+<h3>Film</h3>
+{/* ONE ROW */}
+<Row>
+<Col>
+<p>The Title of the Film</p>
+</Col>
+<Col>
+<p>Lead</p>
+</Col>
+<Col>
+<p>Where/Director</p>
+</Col>
+</Row>
+{/* END OF ROW */}
+{/* ONE ROW */}
+<Row>
+<Col>
+<p>The Title of the Film</p>
+</Col>
+<Col>
+<p>Lead</p>
+</Col>
+<Col>
+<p>Where/Director</p>
+</Col>
+</Row>
+{/* END OF ROW */}
+{/* ONE ROW */}
+<Row>
+<Col>
+<p>The Title of the Film</p>
+</Col>
+<Col>
+<p>Lead</p>
+</Col>
+<Col>
+<p>Where/Director</p>
+</Col>
+</Row>
+{/* END OF ROW */}
+
+<hr/>
+
+<h3>Theater</h3>
+{/* ONE ROW */}
+<Row>
+<Col>
+<p>The Title of the Show
+</p>
+</Col>
+<Col>
+<p>Lead</p>
+</Col>
+<Col>
+<p>Where/Director</p>
+</Col>
+</Row>
+{/* END OF ROW */}
+{/* ONE ROW */}
+<Row>
+<Col>
+<p>The Title of the Show
+</p>
+</Col>
+<Col>
+<p>Lead</p>
+</Col>
+<Col>
+<p>Where/Director</p>
+</Col>
+</Row>
+{/* END OF ROW */}
+{/* ONE ROW */}
+<Row>
+<Col>
+<p>The Title of the Show
+</p>
+</Col>
+<Col>
+<p>Lead</p>
+</Col>
+<Col>
+<p>Where/Director</p>
+</Col>
+</Row>
+{/* END OF ROW */}
+
+<hr/>
+
+<h3>Training</h3>
+{/* ONE ROW */}
+
+    <Col>
+<Row>
+<p>Acting : ________________
+</p>
+</Row>
+
+<Row>
+<p>Voice : ________________</p>
+</Row>
+
+<Row>
+<p>Dance : ________________</p>
+</Row>
+</Col>
+{/* END OF ROW */}
+
+<hr/>
+
+<h3>Skills</h3>
+
+<Row>
+    <Col>
+    <p>Here are some skills that you have.</p>
+    </Col>
+</Row>
+</Col>
+</Jumbotron>
+
+</Row>
+
+
+</Container>
+
 
 
 </>
