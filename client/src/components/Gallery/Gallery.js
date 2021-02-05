02530d5cf91f7ecd5a325796afda6248af1f4b22
James Lamb <jameslamb13@gmail.com>
2021-02-05 01:55:13 -0500

    gallery pictures have media queries.

--- client/src/components/Gallery/Gallery.js
+++ client/src/components/Gallery/Gallery.js
@@ -18,7 +18,7 @@ function Gallery() {
       <br/>
       <br/>
       <br/>
-      <Container>
+      <Container className="gallerySetUp1">
             <Row>
 <Col>
 
@@ -61,6 +61,54 @@ function Gallery() {
 
             </Row>
         
+        
+        
+        
+       
+        
+        </Container>
+
+       
+      <Container className="gallerySetUp2">
+            <Row>
+
+
+<Image src="/images/img_4358.jpeg" alt="Hannah Ryen" className="galleryPics"></Image>
+
+</Row>
+<Row>
+<Image src="/images/702AA743-1BF8-4834-9CB6-129DF0AB2406.JPG" alt="Hannah Ryen" className="galleryPics"></Image>
+
+</Row>
+<Row>
+<Image src="/images/img_4368.jpeg" alt="Hannah Ryen" className="galleryPics"></Image>
+
+</Row>
+            
+            <Row>
+            <Image src="/images/img_4373.jpeg" alt="Hannah Ryen" className="galleryPics"></Image>
+</Row>
+<Row>
+<Image src="/images/1FB77BAF-E59E-4BDE-979A-CDCB702D30C0.JPG" alt="Hannah Ryen" className="galleryPics"></Image>
+</Row>
+<Row>
+<Image src="/images/img_4379.jpeg" alt="Hannah Ryen" className="galleryPics"></Image>
+
+</Row>
+
+            
+            <Row>
+            
+</Row>
+<Row>
+<Image src="/images/A8DB8D68-A494-4001-ACE6-113FE22400D1.JPG" alt="Hannah Ryen" className="galleryPics"></Image>
+</Row>
+<Row>
+
+
+</Row>
+
+            
         
         
         
