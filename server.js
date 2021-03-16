e041b5f9e38b6976c77d058de923e57663ba4597
James Lamb <jameslamb13@gmail.com>
2021-03-16 14:50:51 -0400

    taking out router.

--- server.js
+++ server.js
@@ -9,15 +9,11 @@ const PORT = process.env.PORT || 3001;
 // --- Middleware --- //
 
 
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
 }
 
 
