72f510df8f700f6ff4de1fb9f5f88921687d9f80
James Lamb <jameslamb13@gmail.com>
2021-03-16 01:42:14 -0400

    app.js basename.

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ function App() {
   
   return (
     <>
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router basename="lambaa94.github.io/Acting-Portfolio">
     <Navtop/>
     <Route exact path="/" component={Home} />
     <Route exact path="/about" component={About} />
