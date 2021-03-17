4c53919e48048a6b9948a70904f7b5108ae109dd
James Lamb <jameslamb13@gmail.com>
2021-03-17 02:32:48 -0400

    adding client/build to server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
 
