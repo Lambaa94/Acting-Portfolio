964e6f0b5c437c42f0d5f41a824c7682d0abc4e5
James Lamb <jameslamb13@gmail.com>
2021-03-17 02:38:28 -0400

    deleting .env production if statement from server.

--- server.js
+++ server.js
@@ -12,12 +12,12 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === "production") {
+
   app.use(express.static(path.join(__dirname, 'client/build')));
   app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
-}
+
 
 
 // --- End of Middleware --- //
