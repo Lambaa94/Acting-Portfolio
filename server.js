411c50843788e9f6dbb009728edb1eeeb555a10d
James Lamb <jameslamb13@gmail.com>
2021-03-17 02:35:04 -0400

    trying to debug the same server.js problem.

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, 'client/build')));
   app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
