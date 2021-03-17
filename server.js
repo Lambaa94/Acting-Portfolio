560d3515306e4e624c9137c98505c8f0f7b41aec
James Lamb <jameslamb13@gmail.com>
2021-03-17 02:30:40 -0400

    adding app.get to server.

--- server.js
+++ server.js
@@ -13,7 +13,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
-  app.use("static", express.static("client/build/"));
+  app.use(express.static("client/build"));
+  app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
 }
 
 
