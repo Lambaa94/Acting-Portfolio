a0fc8ba101f300f29a303f6a07ec1127f5361c1b
James Lamb <jameslamb13@gmail.com>
2021-03-18 17:31:21 -0400

    adding direct url to gallery page.

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -22,15 +22,15 @@ function Gallery() {
             <Row>
 <Col>
 
-<Image src="/images/img_4358.jpeg" alt="Hannah Ryen" className="galleryPics"></Image>
+<Image src="https://github.com/Lambaa94/Acting-Portfolio/blob/main/public/images/img_4358.jpeg" alt="Hannah Ryen" className="galleryPics"></Image>
 
 </Col>
 <Col>
-<Image src="/images/702AA743-1BF8-4834-9CB6-129DF0AB2406.JPG" alt="Hannah Ryen" className="galleryPics"></Image>
+<Image src="https://github.com/Lambaa94/Acting-Portfolio/blob/main/public/images/702AA743-1BF8-4834-9CB6-129DF0AB2406.JPG" alt="Hannah Ryen" className="galleryPics"></Image>
 
 </Col>
 <Col>
-<Image src="/images/img_4368.jpeg" alt="Hannah Ryen" className="galleryPics"></Image>
+<Image src="https://github.com/Lambaa94/Acting-Portfolio/blob/main/public/images/img_4368.jpeg" alt="Hannah Ryen" className="galleryPics"></Image>
 
 </Col>
             </Row>
@@ -39,10 +39,10 @@ function Gallery() {
             <Image src="/images/img_4373.jpeg" alt="Hannah Ryen" className="galleryPics"></Image>
 </Col>
 <Col>
-<Image src="/images/1FB77BAF-E59E-4BDE-979A-CDCB702D30C0.JPG" alt="Hannah Ryen" className="galleryPics"></Image>
+<Image src="https://github.com/Lambaa94/Acting-Portfolio/blob/main/public/images/1FB77BAF-E59E-4BDE-979A-CDCB702D30C0.JPG" alt="Hannah Ryen" className="galleryPics"></Image>
 </Col>
 <Col>
-<Image src="/images/img_4379.jpeg" alt="Hannah Ryen" className="galleryPics"></Image>
+<Image src="https://github.com/Lambaa94/Acting-Portfolio/blob/main/public/images/img_4379.jpeg" alt="Hannah Ryen" className="galleryPics"></Image>
 
 </Col>
 
@@ -52,7 +52,7 @@ function Gallery() {
             
 </Col>
 <Col>
-<Image src="/images/A8DB8D68-A494-4001-ACE6-113FE22400D1.JPG" alt="Hannah Ryen" className="galleryPics"></Image>
+<Image src="https://github.com/Lambaa94/Acting-Portfolio/blob/main/public/images/A8DB8D68-A494-4001-ACE6-113FE22400D1.JPG" alt="Hannah Ryen" className="galleryPics"></Image>
 </Col>
 <Col>
 
@@ -73,15 +73,15 @@ function Gallery() {
             <Row>
 
 
-<Image src="/images/img_4358.jpeg" alt="Hannah Ryen" className="galleryPics"></Image>
+<Image src="https://github.com/Lambaa94/Acting-Portfolio/blob/main/public/images/img_4358.jpeg" alt="Hannah Ryen" className="galleryPics"></Image>
 
 </Row>
 <Row>
-<Image src="/images/702AA743-1BF8-4834-9CB6-129DF0AB2406.JPG" alt="Hannah Ryen" className="galleryPics"></Image>
+<Image src="https://github.com/Lambaa94/Acting-Portfolio/blob/main/public/images/702AA743-1BF8-4834-9CB6-129DF0AB2406.JPG" alt="Hannah Ryen" className="galleryPics"></Image>
 
 </Row>
 <Row>
-<Image src="/images/img_4368.jpeg" alt="Hannah Ryen" className="galleryPics"></Image>
+<Image src="https://github.com/Lambaa94/Acting-Portfolio/blob/main/public/images/img_4368.jpeg" alt="Hannah Ryen" className="galleryPics"></Image>
 
 </Row>
             
@@ -89,10 +89,10 @@ function Gallery() {
             <Image src="/images/img_4373.jpeg" alt="Hannah Ryen" className="galleryPics"></Image>
 </Row>
 <Row>
-<Image src="/images/1FB77BAF-E59E-4BDE-979A-CDCB702D30C0.JPG" alt="Hannah Ryen" className="galleryPics"></Image>
+<Image src="https://github.com/Lambaa94/Acting-Portfolio/blob/main/public/images/1FB77BAF-E59E-4BDE-979A-CDCB702D30C0.JPG" alt="Hannah Ryen" className="galleryPics"></Image>
 </Row>
 <Row>
-<Image src="/images/img_4379.jpeg" alt="Hannah Ryen" className="galleryPics"></Image>
+<Image src="https://github.com/Lambaa94/Acting-Portfolio/blob/main/public/images/img_4379.jpeg" alt="Hannah Ryen" className="galleryPics"></Image>
 
 </Row>
 
@@ -101,7 +101,7 @@ function Gallery() {
             
 </Row>
 <Row>
-<Image src="/images/A8DB8D68-A494-4001-ACE6-113FE22400D1.JPG" alt="Hannah Ryen" className="galleryPics"></Image>
+<Image src="https://github.com/Lambaa94/Acting-Portfolio/blob/main/public/images/A8DB8D68-A494-4001-ACE6-113FE22400D1.JPG" alt="Hannah Ryen" className="galleryPics"></Image>
 </Row>
 <Row>
 
