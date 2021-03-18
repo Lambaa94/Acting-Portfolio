981920e8c4aa9fcff12ac81dfc687f4b1de2faea
James Lamb <jameslamb13@gmail.com>
2021-03-18 13:25:20 -0400

    deleting repo name from homepage in package.json and leaving the basename to say the repo name.

--- server.js
+++ server.js
@@ -12,12 +12,9 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-  app.use(express.static(path.join(__dirname, 'client/build')));
-  app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
-
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
 
 // --- End of Middleware --- //
